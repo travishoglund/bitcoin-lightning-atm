18627fab60f230b049d74430ec2818f407e1d584
Travis Hoglund <travishoglund@Traviss-MacBook-Pro.local>
2023-05-24 21:15:00 -0500

    Adding libraries and PLN currency config

---
+++ multi-coin/standard/arduino-code/settings-PLN.ino
@@ -0,0 +1,6 @@
+String baseURLATM = "https://legend.lnbits.com/lnurldevice/api/v1/lnurl/DEVICE_ID";
+String secretATM = "SECRET";
+String currencyATM = "PLN";
+float coinValues[3] = {1.00, 2.00, 5.00};
+int charge = 10; // % you will charge people for service, set in LNbits extension
+float maxAmount = 50.00; // max amount per withdraw
